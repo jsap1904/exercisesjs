df607b770d91e0fef710be64bd859fadfd8f6d44
jsap1904 <sapinoso.j@gmail.com>
2017-07-02 20:57:47 -0700

    update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ var mikeAge = 30
 var mikeHeight = 182
 var johnScore = johnHeight + (johnAge * 5)
 var mikeScore = mikeHeight + (mikeAge * 5)
+
+var game = function() {
+	if(johnScore > mikeScore)
+		return ("John Wins with " + johnScore + "points!");
+}
+if (mikeScore > johnScore) {
+	return ("Mike Wins with " + mikeScore + "points!");
+}
+if (mikeScore === johnScore) {
+	return ("DRAW!")
+}
+else {
+	return("try again!")
+}
