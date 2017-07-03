1baad7f839ded3a7e2675e73e80cc1ac82ea8533
jsap1904 <sapinoso.j@gmail.com>
2017-07-02 20:44:30 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var johnAge = 25
-var johnHeight = 70
+var johnHeight = 170
 var mikeAge = 30
-var mikeHeight = 66
-
+var mikeHeight = 182
+var johnScore = johnHeight + (johnAge * 5)
+var mikeScore = mikeHeight + (mikeAge * 5)
