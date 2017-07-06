a264abc98b7cac2d1a9b639b3f03515eafbebc66
jsap1904 <sapinoso.j@gmail.com>
2017-07-05 22:25:25 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-var johnAge = 25
-var johnHeight = 170
-var mikeAge = 30
-var mikeHeight = 182
-var johnScore = johnHeight + (johnAge * 5)
-var mikeScore = mikeHeight + (mikeAge * 5)
+var johnAge = 25;
+var johnHeight = 200;
+var mikeAge = 30;
+var mikeHeight = 180;
+var johnScore = johnHeight + 5 * johnAge;
+var mikeScore = mikeHeight + 5 * mikeAge;
+var annAge = 35;
+var annHeight = 163;
+var annScore = annHeight + 5 * annAge;
 
-var game = function() {
-	if(johnScore > mikeScore)
-		return ("John Wins with " + johnScore + "points!");
+
+	if (johnScore > mikeScore && johnScore > annScore) {
+		console.log ("John Wins with " + johnScore + " points!");
+	}
+
+else if (mikeScore > johnScore && mikeScore > annScore) {
+	console.log ("Mike Wins with " + mikeScore + " points!");
 }
-if (mikeScore > johnScore) {
-	return ("Mike Wins with " + mikeScore + "points!");
+else if (mikeScore === johnScore || annScore === johnScore || annScore === mikeScore) {
+	console.log ("DRAW!")
 }
-if (mikeScore === johnScore) {
-	return ("DRAW!")
-}
-else {
-	return("try again!")
+else if (annScore > johnScore && annScore > mikeScore) {
+	console.log ("Ann Wins with " + annScore + " points!");
 }
