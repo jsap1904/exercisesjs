998293ad40da0e184c793826d4a23c540480159b
jsap1904 <sapinoso.j@gmail.com>
2017-07-06 17:03:32 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
-var johnAge = 25;
-var johnHeight = 200;
-var mikeAge = 30;
-var mikeHeight = 180;
-var johnScore = johnHeight + 5 * johnAge;
-var mikeScore = mikeHeight + 5 * mikeAge;
-var annAge = 35;
-var annHeight = 163;
-var annScore = annHeight + 5 * annAge;
+//exercise 1
+
+// var johnAge = 25;
+// var johnHeight = 200;
+// var mikeAge = 30;
+// var mikeHeight = 180;
+// var johnScore = johnHeight + 5 * johnAge;
+// var mikeScore = mikeHeight + 5 * mikeAge;
+// var annAge = 35;
+// var annHeight = 163;
+// var annScore = annHeight + 5 * annAge;
 
 
-	if (johnScore > mikeScore && johnScore > annScore) {
-		console.log ("John Wins with " + johnScore + " points!");
-	}
+// 	if (johnScore > mikeScore && johnScore > annScore) {
+// 		console.log ("John Wins with " + johnScore + " points!");
+// 	}
 
-else if (mikeScore > johnScore && mikeScore > annScore) {
-	console.log ("Mike Wins with " + mikeScore + " points!");
-}
-else if (mikeScore === johnScore || annScore === johnScore || annScore === mikeScore) {
-	console.log ("DRAW!")
-}
-else if (annScore > johnScore && annScore > mikeScore) {
-	console.log ("Ann Wins with " + annScore + " points!");
+// else if (mikeScore > johnScore && mikeScore > annScore) {
+// 	console.log ("Mike Wins with " + mikeScore + " points!");
+// }
+// else if (mikeScore === johnScore || annScore === johnScore || annScore === mikeScore) {
+// 	console.log ("DRAW!")
+// }
+// else if (annScore > johnScore && annScore > mikeScore) {
+// 	console.log ("Ann Wins with " + annScore + " points!");
+// }
+
+
+//exercise 2
+
+var years = [1980, 1985, 1990, 1995, 2000, 2017]
+var ages = []
+var fullAges = []
+
+for(var i = 0; i > years.length; i++) {
+	ages[i] = 2017 - years[i];
 }
